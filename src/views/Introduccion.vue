805ef76895736a44c468da0b718a082bb99d276f
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2025-08-15 07:20:13 -0500

    Avance_final

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -28,7 +28,7 @@
       .col-lg-12
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/xmXT1aNjAAA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/3Hpv9_re6D8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
         figcaption Video. Alojamiento, todo un universo por descubrir
 
 </template>
